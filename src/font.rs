77d67f225c73306928ec2f8bd12c05f857908ecc
saltire sable <saltiresable@gmail.com>
2022-01-04 03:09:56 -0500

    Add background box to text renderer; start text in top left corner

--- src/font.rs
+++ src/font.rs
@@ -17,10 +17,14 @@ struct BitmapFont {
     texture_atlas_handle: Handle<TextureAtlas>,
 }
 
+#[derive(Default)]
 struct BitmapText {
     text: String,
     font: String,
-    transform: Transform,
+    position: Vec3,
+    size: Vec2,
+    padding: f32,
+    background_color: Color,
 }
 
 fn setup(
@@ -43,12 +47,17 @@ fn setup(
     commands.spawn().insert(BitmapText {
         text: "Pizza!!".to_string(),
         font: "GeeBee".to_string(),
-        transform: Transform::from_xyz(-220., -115., 2.),
+        position: Vec3::new(0., 0., 2.),
+        size: Vec2::new(100., 20.),
+        padding: 2.,
+        background_color: Color::BLACK,
+        ..Default::default()
     });
 }
 
 fn render_text(
     mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
     fonts: Query<&BitmapFont>,
     texts: Query<&BitmapText, Added<BitmapText>>,
 ) {
@@ -57,10 +66,15 @@ fn render_text(
             if font.name == text.font {
                 commands
                     .spawn_bundle(SpriteBundle {
-                        transform: text.transform,
+                        material: materials.add(text.background_color.into()),
+                        transform: Transform::from_translation(text.position),
+                        sprite: Sprite::new(text.size),
                         ..Default::default()
                     })
                     .with_children(|parent| {
+                        let offset_x = (font.size.x - text.size.x) / 2. + text.padding;
+                        let offset_y = (text.size.y - font.size.y) / 2. - text.padding;
+
                         for (i, b) in text.text.to_uppercase().bytes().enumerate() {
                             parent.spawn_bundle(SpriteSheetBundle {
                                 texture_atlas: font.texture_atlas_handle.clone(),
@@ -69,9 +83,9 @@ fn render_text(
                                     ..Default::default()
                                 },
                                 transform: Transform::from_xyz(
-                                    font.size.x * (i as f32 + 0.5),
-                                    font.size.y * 0.5,
-                                    0.),
+                                    offset_x + font.size.x * i as f32,
+                                    offset_y,
+                                    1.),
                                 ..Default::default()
                             });
                         }
